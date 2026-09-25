b6a714bae09d99c49d529511e6835a01b1ed3e59
rendrixstudio <rendrixstudio@gmail.com>
2026-09-26 01:08:49 +0530

    comm

--- README.md
+++
@@ -1,1 +0,0 @@
-# rendrixstudio.github.io
--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: "/",   // root domain, not a subfolder
-  plugins: [react()],
-});
